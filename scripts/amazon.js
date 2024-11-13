0ac2a4c6d8f39347cadf420690242ddbad4381cb
karthikmadala vs <madalakarthiknaidu3@gmail.com>
2024-11-13 12:26:10 +0530

    make the cart quantity interactive

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -71,10 +71,15 @@ let productsHTML='';
               quantity: 1
              });
           }
+          let cartQuantity = 0;
 
+          cart.forEach((item) => {
+            cartQuantity += item.quantity;
 
-       
-       console.log(cart);
+          });
+
+          document.querySelector('.js-cart-quantity').innerHTML = cartQuantity;
+     
     });
 
   });
