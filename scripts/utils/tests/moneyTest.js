1ae8c162d6771ababa8797083b0d6701f84703ca
karthikmadala <madalakarthiknaidu3@gmail.com>
2025-01-08 15:50:30 +0530

    jasmine-interactive

---
+++ scripts/utils/tests/moneyTest.js
@@ -0,0 +1,19 @@
+import {formatCurrency} from "../money.js";
+
+if(
+formatCurrency(2095)==="20.95"){
+    console.log("passed");
+    
+}
+else{
+    console.log("failed");
+    
+}
+
+if(formatCurrency(0)==="0.00"){
+    console.log("passes");
+}
+else{
+    console.log("failed");
+    
+}
