afa9e8e7237900d895766dac446395690117f272
karthikmadala <madalakarthiknaidu3@gmail.com>
2025-01-23 16:53:06 +0530

    place order button interactive

--- scripts/checkout/paymentSummary.js
+++ scripts/checkout/paymentSummary.js
@@ -2,6 +2,7 @@ import { cart } from "../../data/cart.js";
 import { getProduct } from "../../data/products.js";
 import { getDeliveryOption } from "../../data/deliveryOptions.js";
 import formatCurrency from "../utils/money.js";
+import { addOrder } from "../../data/orders.js";
 
 export function renderPaymentSummary() {
   // Initialize total variables
@@ -59,7 +60,7 @@ export function renderPaymentSummary() {
       <div class="payment-summary-money">$${formatCurrency(totalCents)}</div>
     </div>
 
-    <button class="place-order-button button-primary">
+    <button class="place-order-button button-primary js-place-order">
       Place your order
     </button>
   `;
@@ -71,4 +72,24 @@ export function renderPaymentSummary() {
   } else {
     console.error("Payment summary container not found.");
   }
+  document.querySelector('.js-place-order').addEventListener('click', async ()=>{
+    try{
+      const response = await fetch('https://supersimplebackend.dev/orders',{
+        method:'POST',headers:{
+          'Content-Type':'application/json'
+        },
+        body: JSON.stringify({
+          cart:cart
+        })
+      });
+      const order = await response.json();
+      addOrder(order);
+
+    }catch{
+      console.log('Unexpected error , Try again later.');
+      
+    }
+    window.location.herf='orders.html';
+    
+  });
 }
